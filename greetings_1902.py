aa78a6cbf9d5b169f1e535b5a9b18db13c053ec3
Michael Lozickii <worldspawn.web@gmail.com>
2025-02-19 01:34:20 +0300

    refactor: 19_02

--- greetings_1902.py
+++ greetings_1902.py
@@ -2,57 +2,61 @@
 # using specific Python math functions.
 
 import math
+from sympy import symbols, simplify, Eq, solve
 
 def arithmetic_remainder(num1, num2):
     trunc_num = math.trunc(num1 / num2)
     num3 = trunc_num * num2
     output = num1 - num3
     
-    return print(output)
+    return output
 
     # Complex
         # output = num1 - math.trunc(num1 / num2) * num2
         # return print(output)
         
     # Optimized
-        #
+        # return num1 % num2
     
 def percentage_from(num, percent):
-    return print(math.trunc(num * percent / 100))
+    return (percent / 100) * num
 
 def rectangle_area(a, b):
-    return print(a * b)
+    return a * b
 
 def rectangle_hypotenuse(leg1, leg2):
-    squareHypotenuse = (leg1 * leg1) + (leg2 * leg2)
-    return print(math.trunc(math.sqrt(squareHypotenuse)))
+    return math.sqrt(leg1**2 + leg2**2)
 
 def sum_from_one(n):
-    sum = 1
-    for i in range(n):
-        sum += i
-    return print(sum)
+    return n * (n + 1) // 2 # (arithmetic progression)
+
+def algebra_simplify():
+    x, y = symbols('x y')
+    expression = (x + y)**2 - (x - y)**2
+    simplified_expression = simplify(expression)
+    return simplified_expression
+
+def solve_equation():
+    x = symbols('x')
+    equation = Eq(4*x - 7, 2*x + 9)
+    solution = solve(equation, x)
+    return solution[0]
 
 def main():
     # Chapter No.1
-    arithmetic_remainder(2023, 13)
-    percentage_from(800, 15)
+    print("Остаток от деления:", arithmetic_remainder(2023, 13))
+    print("15% от 800:", percentage_from(800, 15))
 
     # Chapter No.2
-    # original_sentence = "(x + y)^2 - (x - y)^2"
-    # simplified_sentence = "x^2 + 2xy + y^2 - x^2 - 2xy + y^2"
-    print("2y^2")
-    
-    # original_sentence_2 = "4x - 7 = 2x + 9"
-    # simplified_sentence_2 = "4x - 2x = 9 + 7" = "2x = 16"
-    print("x = 8")
+    print("Упрощенное выражение:", algebra_simplify())
+    print("Решение уравнения:", solve_equation())
     
     # Chapter No.3
-    rectangle_area(6, 10)
-    rectangle_hypotenuse(5, 12)
+    print("Площадь прямоугольника:", rectangle_area(6, 10))
+    print("Гипотенуза треугольника:", rectangle_hypotenuse(5, 12))
     
     # Chapter No.4
-    sum_from_one(5)
+    print("Сумма чисел от 1 до 14:", sum_from_one(14))
 
 if __name__ == "__main__":
     main()
